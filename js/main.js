c9517b84abc981d76e971d736e3db3769c67d2ce
kayheypark <59360980+kayheypark@users.noreply.github.com>
2020-06-23 03:10:45 +0900

    calendar -ing

--- js/main.js
+++ js/main.js
@@ -3,19 +3,34 @@ window.onload = function() {
     //시간정보 불러옴
     setInterval(function(){
         var d = new Date();
+        var sysYr = d.getFullYear();
+        var sysMth = d.getMonth()-1;
         var sysHr = addZero(d.getHours(),2);
         var sysMin = addZero(d.getMinutes(),2);
 
+        //상태바 시계영역
         var areaHr = document.getElementById('header-time-hr');
         var areaMin = document.getElementById('header-time-min');
 
+        //홈, 일정 페이지
+        //월, 연도 영역
+        var areaYr = document.getElementById('header-home-time-year');
+        var areaMth = document.getElementById('header-home-time-month');
         
-        var time = function (){
+        var refreshZero = function (){
             areaHr.innerHTML = sysHr;
             areaMin.innerHTML = sysMin;
             addZero(sysHr, 9);
         }
-        time();
+        refreshZero();
+
+        var refresh = function (){
+            areaYr.innerHTML = sysYr;
+            areaMth.innerHTML = sysMth;
+        }
+        refresh();
+
+
     },1000)
 
     function addZero (num, digit) {
@@ -30,5 +45,8 @@ window.onload = function() {
         }
         return zero + num;
     }
+
+
+
     
-}// 독레디 닫기
+}// 온로드 닫기
